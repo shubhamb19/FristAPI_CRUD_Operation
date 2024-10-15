5b11518db5b6d25e029b94b52044f33032cefdd3
shubhamb19 <shubhambhuse19@gmail.com>
2024-10-15 13:41:56 +0530

    Add project files.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,67 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class PizzaController : Controller
+    {
+        public PizzaController()
+        {
+
+        }
+        // for get all pizzas in pizza list
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        // for get single pizza from pizza list
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        { 
+            var Pizza = PizzaService.Get(id);
+            if (Pizza == null) 
+            {
+                return NotFound();
+            }
+            return Pizza;
+        }
+
+        // this http method for ADD Pizza
+        [HttpPost]
+        public IActionResult Create(Pizza pizza) 
+        {
+            PizzaService.Add(pizza);
+
+            return CreatedAtAction(nameof(Get), new {id = pizza.Id}, pizza);
+        }
+
+        // This code will update the pizza and return a result
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            var existingPizza = PizzaService.Get(id); 
+            if (existingPizza == null) 
+            {
+                return NotFound();
+            }
+            pizza.Id = id;
+            PizzaService.Update(pizza);
+             return NoContent();
+        }
+
+        // This code will delete the pizza and return a result
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza == null)
+            {
+                return NotFound();
+            }
+            PizzaService.Delete(id);
+            return NoContent(); 
+        }
+    }
+}
---
+++ Services/PizzaService.cs
@@ -0,0 +1,54 @@
+﻿using ContosoPizza.Models;
+
+namespace ContosoPizza.Services
+{
+    static public class PizzaService
+    {
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1 , Name = "Classic Italian", IsGlutenFree= false},
+                new Pizza { Id =  2 , Name = "Veggie" , IsGlutenFree = true}
+            };
+
+        }
+        //This method is used for get all pizzas in pizza list
+        public static List<Pizza> GetAll() => Pizzas;
+
+        //this method that retrieves a specific pizza from the Pizzas list by its ID
+        public static Pizza Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+        //this method is used for ADD Pizza 
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+        // this method is used for update pizza
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if (index == -1)
+                return;
+
+            Pizzas[index] = pizza;
+        }
+
+        //this method is used for delete pizza 
+        public static void Delete(int id)
+        { 
+            var pizza = Get(id);
+            if (pizza is null) 
+            {
+                return;
+            }
+
+            Pizzas.Remove(pizza);
+        }
+
+    }
+}
